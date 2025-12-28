24e523abbc906374b64cd65293f6518e10c0703d
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-28 22:48:29 +0100

    feat(GED-22): add dynamic form fields

--- src/forms/forms.module.ts
+++ src/forms/forms.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { FormField } from './form-field.entity';
+import { FormFieldsController } from './form-fields.controller';
+import { FormFieldsService } from './form-fields.service';
 import { Form } from './form.entity';
 import { FormsController } from './forms.controller';
 import { FormsService } from './forms.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Form])],
-  controllers: [FormsController],
-  providers: [FormsService],
+  imports: [TypeOrmModule.forFeature([Form, FormField])],
+  controllers: [FormsController, FormFieldsController],
+  providers: [FormsService, FormFieldsService],
   exports: [FormsService],
 })
 export class FormsModule {}
